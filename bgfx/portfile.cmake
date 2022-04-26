eb5f2c393dfe010e0689939bfa1ddcfce6a973bf
jbax86 <jbax86@gmail.com>
2022-04-26 06:22:01 -0700

    add first custom port files to repo

---
+++ bgfx/portfile.cmake
@@ -0,0 +1,160 @@
+# For a list of common variables see https://github.com/microsoft/vcpkg/blob/master/docs/maintainers/vcpkg_common_definitions.md
+
+# Download source packages
+# (bgfx requires bx and bimg source for building)
+
+vcpkg_from_github(OUT_SOURCE_PATH BX_SOURCE_DIR
+    REPO "bkaradzic/bx"
+    HEAD_REF master
+    REF d7ecac17060ab8cf765b21e55a762aa4855dfdb1
+    SHA512 d92144ab43464e6df6cd4f9aff4b4ae73c794e5dad79b6026773faf7afcce36f4552868375dc8d1989145eb5f115e76c3bb75dd6edba10e13ce82479f2cd78b4
+)
+
+vcpkg_from_github(OUT_SOURCE_PATH BIMG_SOURCE_DIR
+    REPO "bkaradzic/bimg"
+    HEAD_REF master
+    REF 663f724186e26caf46494e389ed82409106205fb
+    SHA512 6fcd6d06d0d55a8f573a882bb57b729ee22101314a201bd8f440bdcadbfeb0dca6834302ef7dcfdee785d87b5456d24d2cb630d1d39836b4cd7652a6dc92d5a8
+)
+
+vcpkg_from_github(OUT_SOURCE_PATH SOURCE_DIR
+    REPO "bkaradzic/bgfx"
+    HEAD_REF master
+    REF b8fdc3f8be2dc8d94acfe143933a4267d9525044
+    SHA512 2af323c9a784e78ec116e78d07239f758ed7df9bc31d3c83830f7dd2d7462151700780f40a492527ff6b1fdd259cde7b22b9e59bf4bb100f094dc2084f19ac34
+)
+
+# Move bx source inside bgfx source tree
+set(BX_DIR ${SOURCE_DIR}/.bx)
+file(RENAME ${BX_SOURCE_DIR} "${BX_DIR}")
+set(ENV{BX_DIR} ${BX_DIR})
+
+# Move bimg source inside bgfx source tree
+set(BIMG_DIR ${SOURCE_DIR}/.bimg)
+file(RENAME ${BIMG_SOURCE_DIR} "${BIMG_DIR}")
+set(ENV{BIMG_DIR} ${BIMG_DIR})
+
+# Set custom BGFX configuration
+set(ENV{BGFX_CONFIG} "DEBUG=1:PREFER_DISCRETE_GPU=0:DYNAMIC_INDEX_BUFFER_SIZE=10485760:DYNAMIC_VERTEX_BUFFER_SIZE=31457280")
+
+# Set up GENie (custom project generator)
+set(GENIE_OPTIONS --with-tools)
+
+if(VCPKG_CRT_LINKAGE STREQUAL dynamic)
+    set(GENIE_OPTIONS ${GENIE_OPTIONS} --with-dynamic-runtime)
+endif()
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
+    set(GENIE_OPTIONS ${GENIE_OPTIONS} --with-shared-lib)
+endif()
+
+if(VCPKG_TARGET_ARCHITECTURE STREQUAL x86)
+    set(GENIE_OPTIONS ${GENIE_OPTIONS} --platform=x32)
+elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL x64)
+    set(GENIE_OPTIONS ${GENIE_OPTIONS} --platform=x64)
+elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL arm OR VCPKG_TARGET_ARCHITECTURE STREQUAL arm64)
+    set(GENIE_OPTIONS ${GENIE_OPTIONS} --platform=ARM)
+else()
+    message(WARNING "Architecture may be not supported: ${VCPKG_TARGET_ARCHITECTURE}")
+    set(GENIE_OPTIONS ${GENIE_OPTIONS} --platform=${VCPKG_TARGET_ARCHITECTURE})
+endif()
+
+if(TARGET_TRIPLET MATCHES osx)
+    set(GENIE_OPTIONS ${GENIE_OPTIONS} --os=macosx)
+elseif(TARGET_TRIPLET MATCHES linux)
+    set(GENIE_OPTIONS ${GENIE_OPTIONS} --os=linux)
+elseif(TARGET_TRIPLET MATCHES windows)
+    set(GENIE_OPTIONS ${GENIE_OPTIONS} --os=windows)
+elseif(TARGET_TRIPLET MATCHES uwp)
+    set(GENIE_OPTIONS ${GENIE_OPTIONS} --vs=winstore100)
+endif()
+
+# GENie does not allow cmake+msvc, so we use msbuild in windows
+if(CMAKE_HOST_SYSTEM_NAME STREQUAL "Windows")
+    if(VCPKG_PLATFORM_TOOLSET STREQUAL "v140")
+        set(GENIE_ACTION vs2015)
+    elseif(VCPKG_PLATFORM_TOOLSET STREQUAL "v141")
+        set(GENIE_ACTION vs2017)
+    elseif(VCPKG_PLATFORM_TOOLSET STREQUAL "v142")
+        set(GENIE_ACTION vs2019)
+    elseif(VCPKG_PLATFORM_TOOLSET STREQUAL "v143")
+        set(GENIE_ACTION vs2022)
+    else()
+        message(FATAL_ERROR "Unsupported Visual Studio toolset: ${VCPKG_PLATFORM_TOOLSET}")
+    endif()
+    set(PROJ_FOLDER ${GENIE_ACTION})
+    if(TARGET_TRIPLET MATCHES uwp)
+        set(PROJ_FOLDER ${PROJ_FOLDER}-winstore100)
+    endif()
+else()
+    set(GENIE_ACTION cmake)
+    set(PROJ_FOLDER ${GENIE_ACTION})
+endif()
+
+if(CMAKE_HOST_SYSTEM_NAME STREQUAL "Windows")
+    set(GENIE "${BX_DIR}/tools/bin/windows/genie.exe")
+elseif(CMAKE_HOST_SYSTEM_NAME STREQUAL "Darwin")
+    set(GENIE "${BX_DIR}/tools/bin/darwin/genie")
+elseif(CMAKE_HOST_SYSTEM_NAME STREQUAL "Linux")
+    set(GENIE "${BX_DIR}/tools/bin/linux/genie")
+else()
+    message(FATAL_ERROR "Unsupported host platform: ${CMAKE_HOST_SYSTEM_NAME}")
+endif()
+
+# Run GENie
+
+vcpkg_execute_required_process(
+    COMMAND ${GENIE} ${GENIE_OPTIONS} ${GENIE_ACTION}
+    WORKING_DIRECTORY "${SOURCE_DIR}"
+    LOGNAME "genie-${TARGET_TRIPLET}"
+)
+
+if(GENIE_ACTION STREQUAL cmake)
+    if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
+        set(PROJ bgfx-shared-lib)
+    else()
+        set(PROJ bgfx)
+    endif()
+    vcpkg_configure_cmake(
+        SOURCE_PATH "${SOURCE_DIR}/.build/projects/${PROJ_FOLDER}"
+        PREFER_NINJA
+        OPTIONS_RELEASE -DCMAKE_BUILD_TYPE=Release
+        OPTIONS_DEBUG -DCMAKE_BUILD_TYPE=Debug
+    )
+    vcpkg_install_cmake(TARGET ${PROJ}/all)
+    file(INSTALL "${SOURCE_DIR}/include/bgfx" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+    file(GLOB instfiles
+        "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/${PROJ}/*.a"
+        "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/${PROJ}/*.so"
+    )
+    file(INSTALL ${instfiles} DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
+    file(GLOB instfiles
+        "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/${PROJ}/*.a"
+        "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/${PROJ}/*.so"
+    )
+    file(INSTALL ${instfiles} DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib")
+    file(INSTALL "${SOURCE_DIR}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME "copyright")
+else()
+    vcpkg_install_msbuild(
+        SOURCE_PATH "${SOURCE_DIR}"
+        PROJECT_SUBPATH ".build/projects/${PROJ_FOLDER}/bgfx.sln"
+        LICENSE_SUBPATH "LICENSE"
+        INCLUDES_SUBPATH "include"
+    )
+    # Install shader include file
+    file(INSTALL "${SOURCE_DIR}/src/bgfx_shader.sh" DESTINATION "${CURRENT_PACKAGES_DIR}/include/bgfx" )
+    # Remove redundant files
+    foreach(a bx bimg bimg_decode bimg_encode)
+        foreach(b Debug Release)
+            foreach(c lib pdb)
+                if(b STREQUAL Debug)
+                    file(REMOVE "${CURRENT_PACKAGES_DIR}/debug/lib/${a}${b}.${c}")
+                else()
+                    file(REMOVE "${CURRENT_PACKAGES_DIR}/lib/${a}${b}.${c}")
+                endif()
+            endforeach()
+        endforeach()
+    endforeach()
+endif()
+
+configure_file(${CMAKE_CURRENT_LIST_DIR}/FindBGFX.cmake.in ${CURRENT_PACKAGES_DIR}/share/${PORT}/FindBGFX.cmake @ONLY)
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
